909a0d9ddc146f3e82ed694b3e14bc19fff1d445
Cathal Duffy <cathalduffy@cathals-mbp.home>
2022-08-09 19:30:06 +0100

    Add first navbar

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter, Route, Navigate, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Navigate, Routes, Link } from "react-router-dom";
 import HomePage from './pages/homePage';
 import TeamDetailsPage from './pages/teamDetailsPage';
 
 const App = () => {
   return (
     <BrowserRouter>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        {/* <li>
+          <Link to="/movies/favourites">Favourites</Link>
+        </li> */}
+      </ul>
       <Routes>
         <Route path="/team/:id" element={<TeamDetailsPage/>} />
         <Route path="/" element={<HomePage />} />
